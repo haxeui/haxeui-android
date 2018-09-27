2ea4eeaa0914c21df9f63986914f8265e990939b
Ian Harrigan <ianharrigan@hotmail.com>
2018-09-27 17:39:53 +0200

    clean up

--- haxe/ui/backend/android/behaviours/WidgetText.hx
+++ haxe/ui/backend/android/behaviours/WidgetText.hx
@@ -1,40 +1,23 @@
 package haxe.ui.backend.android.behaviours;
 
-import haxe.ui.core.Behaviour;
+import android.widget.TextView;
 import haxe.ui.core.DataBehaviour;
 import haxe.ui.util.Variant;
-import android.widget.TextView;
+import java.lang.CharSequence;
+import java.lang.StringBuilder;
 
 class WidgetText extends DataBehaviour {
-    /*
-    public override function set(value:Variant) {
-        if (value == null || value.isNull == true) {
-            return;
-        }
-        
-        if (Std.is(_component.view, TextView)) {
-            var textView:TextView = cast(_component.view, TextView);
-            textView.setText(value.toString());
-            _component.invalidateComponentLayout();
-            trace(">>>>>>>>>>>>>>>>>>>>>>>>> " + _component.view.getHeight());
-            //cast(_component.view, android.widget.Button).setText(value);
-        }
-    }
-
-    public override function get():Variant {
-        return null;
-    }
-    */
-    /*
     public override function get():Variant {
         if (Std.is(_component.view, TextView)) {
             var textView:TextView = cast(_component.view, TextView);
-            return textView.getText();
+            var ch:CharSequence = textView.getText();
+            var sb:StringBuilder = new StringBuilder();
+            sb.append(ch);
+            return sb.toString();
         }
         
         return null;
     }
-    */
     
     public override function validateData() {
         if (Std.is(_component.view, TextView)) {
